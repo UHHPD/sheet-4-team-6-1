5cdeaedda57cd75cde98f7656239d4afe1562211
CarinaBSC <carina.pruente@web.de>
2020-12-16 16:40:24 +0100

    Add files via upload

--- Data.hh
+++ Data.hh
@@ -10,16 +10,23 @@ class Data {
 
   unsigned int size() const { return m_data.size(); }
   double measurement(int i) const { return m_data[i]; }
-  double binCenter(int i) const { return 0; }
-  double binLow(int i) const { return 0; }
-  double binHigh(int i) const { return 0; }
-  double error(int i) const { return 0; }
+  double binCenter(int i) const { return double((m_bins[i]+m_bins[i+1])/2); }
+  double binLow(int i) const { return double(m_bins[i]); }
+  double binHigh(int i) const { return double(m_bins[i+1]); }
+  double error(int i) const { return (m_error[i]); }
+
+  int checkCompatibility(const Data& in, int n);
+  double x2test();
+  int check(int n);
+
 
  private:
   Data() {}  // disallow empty data set
   void assertSizes();
   std::vector<double> m_data;
   std::vector<double> m_bins;
+  std::vector<double> m_error;
+
 };
 
 #endif
